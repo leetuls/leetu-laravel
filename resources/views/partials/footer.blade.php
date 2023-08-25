4647dfee79dbaebbb65e7f1b56718513f28b226c
leetu <lyngoctuls1995@gmail.com>
2023-08-25 22:40:47 +0700

    25082023 create adminlte

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2013-2024 <a href="https://tuduong.info.vn" target="_blank">Lee Tu Entertainment</a>.</strong> All rights reserved.
+  </footer>
